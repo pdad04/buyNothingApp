57c14f31b33065493be4e26eb7dc77984a539aae
Andre <andre_dean@me.com>
2021-01-03 11:41:00 -0800

    Installed mongo and dotenv npm packages. Configured MongoDB connection and uninstalled npm config package.

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
+
+// Create MongoDB client
+const client = new MongoClient(process.env.DB_CONNECTION, {
+  useUnifiedTopology: true
+});
+
+// Connect client to DB
+const connectDB = async () => {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    
+    console.log("Connected successfully to DB");
+  } catch (error) {
+    console.error(error.message);
+    process.exit(-1);
+  }
+}
+
+// Connect to DB
+connectDB();
 
 app.get("/", (req, res ) => res.send({ msg: "API Running" }));
 
